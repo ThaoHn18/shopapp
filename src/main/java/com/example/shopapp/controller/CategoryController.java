d963703bc14fcb65582c9723851131918be5f75d
ThaoHN <hoangngocthaonazca@baoviet.com.vn>
2025-02-08 15:49:57 +0700

    init project

---
+++ src/main/java/com/example/shopapp/controller/CategoryController.java
@@ -0,0 +1,49 @@
+package com.example.shopapp.controller;
+
+import com.example.shopapp.dtos.CategoryDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/categories")
+//@Validated
+public class CategoryController {
+    @GetMapping("")
+    public ResponseEntity<String> getAllCaregories(
+            @RequestParam("page") int page,
+            @RequestParam("limit") int limit
+    ){
+        return ResponseEntity.ok(String.format("getAllCategories, page=%d, limit=%d",page,limit));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> createCategorie(
+            @Valid @RequestBody  CategoryDTO categoryDTO,
+            BindingResult result
+            ){
+        if (result.hasErrors()){
+            List<String> errorMessages=result.getFieldErrors()
+                    .stream()
+                    .map(FieldError:: getDefaultMessage)
+                    .toList();
+            return ResponseEntity.badRequest().body(errorMessages);
+        }
+        return ResponseEntity.ok("tao thanh cong");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCategory(@PathVariable Long id){
+        return ResponseEntity.ok("update thanh cong");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id){
+        return ResponseEntity.ok("xoa thanh cong");
+    }
+}
